5f97788b0f35f7106d9cea2b66307129eb6c163a
Farheen Begum <farheenbegum312@gmail.com>
2026-05-14 19:23:40 +0530

    trigger build

--- calculator/src/main/java/calculator/CalculatorMain.java
+++ calculator/src/main/java/calculator/CalculatorMain.java
@@ -4,6 +4,8 @@ public class CalculatorMain {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Testing Jenkins Trigger");
+		
 		Calculator c1 = new Calculator();
         c1.add(10, 20);
         c1.sub(10, 20);
